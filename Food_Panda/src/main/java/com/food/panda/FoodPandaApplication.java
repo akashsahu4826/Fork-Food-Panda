2038720bc57b805174776b05520c15565e644154
Mahendra1445 <104969745+Mahendra1445@users.noreply.github.com>
2022-12-07 10:45:24 +0530

    project structure upload
    project structure upload

---
+++ Food_Panda/src/main/java/com/food/panda/FoodPandaApplication.java
@@ -0,0 +1,13 @@
+package com.food.panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodPandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodPandaApplication.class, args);
+	}
+
+}
